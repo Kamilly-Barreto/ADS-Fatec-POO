e1476fdefbc8807674d80a93e0125129164239ba
Kamilly-Barreto <kamilly.sousab@gmail.com>
2025-03-16 22:17:50 -0300

    Aula 04 - Java EE

---
+++ src/java/servlets/ApiLoteriaServlet.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Random;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Finahabit
+ */
+@WebServlet(name = "ApiLoteriaServlet", urlPatterns = {"/loteria.json"})
+public class ApiLoteriaServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            JSONObject obj = new JSONObject();
+            JSONArray numeros = new JSONArray();
+            Date data = new Date();
+            Random rd = new Random();
+            
+            int num = rd.nextInt(100);
+            
+            for (int i = 0; i <= 6; i++) {
+                if (numeros.isEmpty()) {
+                    numeros.put(num);
+                }
+                else {
+                    while (numeros.toString().contains(String.valueOf(num))) {
+                        num = rd.nextInt(100);
+                    }
+                    
+                    numeros.put(num); 
+                    
+                                       
+                }
+            }
+            
+            obj.append("data_hora", data.toString());
+            obj.append("numeros", numeros);
+            
+            out.println(obj.toString());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
